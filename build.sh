a1a6c6461217243ef2521bc9d3dc126b0ca56b13
fajar4561 <maulanafajar751@gmail.com>
2021-07-18 17:47:21 +0700

    update

--- build.sh
+++ build.sh
@@ -57,14 +57,14 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
 NAMA=Signature
-JENIS=CAF
+JENIS=HMP
 VARIAN=STOCK
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc'
-COMPILER=gcc
+COMPILER=clang
 
 # Kernel is LTO
 LTO=0
