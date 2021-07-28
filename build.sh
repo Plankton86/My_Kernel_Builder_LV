2021f4fc933db0d3f8548c8faf51c756f734b8ae
fajar4561 <maulanafajar751@gmail.com>
2021-07-29 00:07:31 +0700

    welleh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@
  # See the License for the specific language governing permissions and
  # limitations under the License.
  #
-
+ 
 #Kernel building script
 
 # Function to show an informational message
