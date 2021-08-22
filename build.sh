e16e09cb136721aa6aa213ed5ac1333247000546
BENKz29 <79386840+BENKz29@users.noreply.github.com>
2021-08-22 10:19:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,15 +50,15 @@ DEVICE="X00T"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=ruega_defconfig
+DEFCONFIG=electroperf_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=Ruega
-JENIS=HMP
-VARIAN=CAF
+NAMA=Fox
+JENIS=LTO
+VARIAN=HMP
 # Build Type
 BUILD_TYPE="Nightly"
 
@@ -67,7 +67,7 @@ BUILD_TYPE="Nightly"
 COMPILER=gcc
 
 # Kernel is LTO
-LTO=0
+LTO=1
 
 # Specify linker.
 # 'ld.lld'(default)
@@ -143,8 +143,8 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	elif [ $COMPILER = "gcc" ]
 	then
 		msg "|| Cloning GCC  ||"
-		git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 -b master $KERNEL_DIR/gcc64
-		git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 -b master $KERNEL_DIR/gcc32
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1
+		git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
 
 	elif [ $COMPILER = "clangxgcc" ]
 	then
@@ -152,8 +152,8 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 		git clone --depth=1 https://github.com/fajar4561/DragonTC -b 10.0 clang
 
 		msg "|| Cloning GCC ||"
-		git clone --depth=1 https://github.com/Thoreck-project/aarch64-linux-android -b opt-gnu-8.x gcc64 
-		git clone --depth=1 https://github.com/Thoreck-project/arm-linux-androideabi gcc32 
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1 
+		git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1 
 	fi
 
 	# Toolchain Directory defaults to clang-llvm
@@ -164,7 +164,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 		GCC32_DIR=$KERNEL_DIR/gcc32
 
 	msg "|| Cloning Anykernel ||"
-        git clone https://github.com/benkz29/Anykernel3.git -b X00T AnyKernel3
+        git clone https://github.com/beesa03/Anykernel3.git AnyKernel3
 
 	if [ $BUILD_DTBO = 1 ]
 	then
@@ -189,7 +189,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="nobody"
     export KBUILD_BUILD_HOST="android-build"
-    export KBUILD_BUILD_VERSION="23"
+    export KBUILD_BUILD_VERSION="17"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
@@ -203,7 +203,7 @@ exports() {
 		PATH=$TC_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:/usr/bin:$PATH
 	elif [ $COMPILER = "gcc" ]
 	then
-		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-linux-gnu-gcc --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 	fi
 
@@ -328,8 +328,11 @@ build_kernel() {
 	elif [ $COMPILER = "gcc" ]
 	then
 		make -j"$PROCS" O=out \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-				CROSS_COMPILE=aarch64-linux-gnu- "${MAKE[@]}" 2>&1 | tee build.log
+				CROSS_COMPILE_ARM32=arm-eabi- \
+				CROSS_COMPILE=aarch64-elf- \
+				AR=aarch64-elf-ar \
+				OBJDUMP=aarch64-elf-objdump \
+				STRIP=aarch64-elf-strip "${MAKE[@]}" 2>&1 | tee build.log
 	elif [ $COMPILER = "clangxgcc" ]
 	then
 		make -j"$PROCS"  O=out \
@@ -383,7 +386,7 @@ gen_zip() {
 	sed -i "s/kernel.compiler=.*/kernel.compiler=$KBUILD_COMPILER_STRING/g" anykernel.sh
 	sed -i "s/kernel.made=.*/kernel.made=$KBUILD_BUILD_USER @$KBUILD_BUILD_HOST/g" anykernel.sh
 	sed -i "s/kernel.version=.*/kernel.version=$LINUXVER/g" anykernel.sh
-	sed -i "s/message.word=.*/message.word=jangan lupa ngopi sama hudud./g" anykernel.sh
+	sed -i "s/message.word=.*/message.word=jangan lupa ngopi sama udut:v/g" anykernel.sh
 	sed -i "s/build.date=.*/build.date=$DATE2/g" anykernel.sh
 
 
